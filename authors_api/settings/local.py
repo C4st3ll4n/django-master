20adbdec3bef1d34933875bbe16e1913ab6000b8
p_souza <psouza@braspag.com.br>
2022-07-02 23:02:51 -0300

    setup django apps

---
+++ authors_api/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+from .base import env
+
+DEBUG = True
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="THISNTAGOOSSECRETKEY",
+)
+
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "contato@bemlidos.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Bem Lidos"
